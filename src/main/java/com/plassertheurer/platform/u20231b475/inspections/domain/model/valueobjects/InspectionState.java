d5d335af5bef6ede8f02c32ce96166d8fdfa0e1a
Trevor <valentinosolischang@gmail.com>
2025-12-08 22:31:35 -0500

    Add InspectionState enum to represent possible inspection states

---
+++ src/main/java/com/plassertheurer/platform/u20231b475/inspections/domain/model/valueobjects/InspectionState.java
@@ -0,0 +1,10 @@
+package com.plassertheurer.platform.u20231b475.inspections.domain.model.valueobjects;
+
+/**
+ * This enum represents the possible states of the inspection
+ */
+public enum InspectionState {
+  ACCEPTED,
+  FLAGGED,
+  REJECTED
+}
