59593f9ea17180e2cc051d985dbae538a61b32c3
Trevor <valentinosolischang@gmail.com>
2025-12-08 21:06:31 -0500

    Add GetSafetyStandardByParameterQuery record for safety standard retrieval

---
+++ src/main/java/com/plassertheurer/platform/u20231b475/regulations/domain/model/queries/GetSafetyStandardByParameterQuery.java
@@ -0,0 +1,7 @@
+package com.plassertheurer.platform.u20231b475.regulations.domain.model.queries;
+
+/**
+ * Query to retrieve a safety standard by its parameter
+ */
+public record GetSafetyStandardByParameterQuery(String parameter) {
+}
