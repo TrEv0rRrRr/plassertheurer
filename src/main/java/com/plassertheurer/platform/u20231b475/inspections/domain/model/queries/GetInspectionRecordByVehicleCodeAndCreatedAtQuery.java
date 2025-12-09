3678f68f58661b341d9c000fe923cd4fa65e26c5
Trevor <valentinosolischang@gmail.com>
2025-12-08 23:57:54 -0500

    Refactor GetInspectionRecordByVehicleCodeAndCreatedAtQuery to use VehicleCode and LocalDateTime types

--- src/main/java/com/plassertheurer/platform/u20231b475/inspections/domain/model/queries/GetInspectionRecordByVehicleCodeAndCreatedAtQuery.java
+++ src/main/java/com/plassertheurer/platform/u20231b475/inspections/domain/model/queries/GetInspectionRecordByVehicleCodeAndCreatedAtQuery.java
@@ -1,9 +1,11 @@
 package com.plassertheurer.platform.u20231b475.inspections.domain.model.queries;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+
+import com.plassertheurer.platform.u20231b475.inspections.domain.model.valueobjects.VehicleCode;
 
 /**
  * Query to get an inspection record by vehicle code and creation date
  */
-public record GetInspectionRecordByVehicleCodeAndCreatedAtQuery(String vehicleCode, Date createdAt) {
+public record GetInspectionRecordByVehicleCodeAndCreatedAtQuery(VehicleCode vehicleCode, LocalDateTime inspectedAt) {
 }
