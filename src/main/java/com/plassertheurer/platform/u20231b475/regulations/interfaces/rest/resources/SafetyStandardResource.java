26de396cc81eecdb87069221c8dca79b6a87caeb
Trevor <valentinosolischang@gmail.com>
2025-12-08 21:06:49 -0500

    Add SafetyStandardResource record for safety standard parameters

---
+++ src/main/java/com/plassertheurer/platform/u20231b475/regulations/interfaces/rest/resources/SafetyStandardResource.java
@@ -0,0 +1,4 @@
+package com.plassertheurer.platform.u20231b475.regulations.interfaces.rest.resources;
+
+public record SafetyStandardResource(String parameter, Double minAcceptableValue, Double maxAcceptableValue) {
+}
